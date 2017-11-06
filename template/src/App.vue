30059f62106029778944ea814c8044d9d59a8a44
macfly <macfly669@gmail.com>
2017-11-06 23:20:56 +0100

    App.vue #40 pipe translate

--- template/src/App.vue
+++ template/src/App.vue
@@ -37,7 +37,7 @@
                 <v-list-tile-action>
                       <v-icon>{{ item.icon }}</v-icon>
                 </v-list-tile-action>
-                    <v-list-tile-content>{{ item.title | translate }}</v-list-tile-content>
+                    <v-list-tile-content>{{ item.title }}</v-list-tile-content>
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
