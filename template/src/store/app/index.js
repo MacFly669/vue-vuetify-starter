f60676d455e51aaf2a2f19d11482a8f3975e4024
macfly <macfly669@gmail.com>
2017-11-06 21:18:03 +0100

    fix ESLint error

--- template/src/store/app/index.js
+++ template/src/store/app/index.js
@@ -1,12 +1,13 @@
-{{#vuex}}import state from './state'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{#vuex}}
+import state from './state'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import actions from './actions'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import mutations from './mutations'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import getters from '/getters'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import getters from './getters'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default {
   state,
   actions,
   mutations,
-  getters{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-}
+  getters
+}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
