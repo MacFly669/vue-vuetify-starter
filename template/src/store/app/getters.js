3a3c6f85aaa3582a36f157dde16513d4485deaa5
macfly <macfly669@gmail.com>
2017-11-06 21:18:03 +0100

    fix ESLint error

--- template/src/store/app/getters.js
+++ template/src/store/app/getters.js
@@ -1,7 +1,7 @@
 {{#vuex}}
 const getters = {
-    
-    }
-    
+
+}
+
 export default getters{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
--- template/src/store/index.js
+++ template/src/store/index.js
@@ -11,7 +11,7 @@ const store = new Vuex.Store({
   modules: {
     app{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }
-})
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default store{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
